0812ca5480c6b3135c7647c85f0af21a42ce748f
Chinmay-innovates <chinmaygope69@gmail.com>
2024-10-09 10:49:57 +0530

    add close-poll server action

--- src/components/dialogs/close-poll-dialog.tsx
+++ src/components/dialogs/close-poll-dialog.tsx
@@ -13,53 +13,63 @@ import {
 } from "../ui/alert-dialog";
 import {buttonVariants} from "../ui/button";
 import React from "react";
+import {useAction} from "next-safe-action/hooks";
+import {closePollAction} from "@/lib/actions/close-poll-action";
+import {toast} from "sonner";
 
 type Props = {
     pollId: Poll["id"];
     onSuccess?: () => void;
 } & AlertDialogProps;
 
-export const ClosePollDialog = ({
-                                    pollId,
-                                    onSuccess: handleSuccess,
-                                    ...dialogProps
-                                }: Props) => {
+export const ClosePollDialog =
+    ({pollId, onSuccess: handleSuccess, ...dialogProps}: Props) => {
 
-    const handleClose = (evt: React.MouseEvent) => {
-        evt.preventDefault();
+        const {execute, isExecuting: isFieldDisabled} = useAction(closePollAction, {
+            onError: (err) => {
+                console.error(err);
+                toast.error("Failed to close the poll. Please try again later")
+            },
+            onSuccess: () => {
+                handleSuccess?.();
+                toast.success("Your poll has been successfully closed");
+            }
+        })
 
-        // execute({pollId});
+        const handleClose = (evt: React.MouseEvent) => {
+            evt.preventDefault();
+
+            execute({pollId});
+        };
+
+
+        return (
+            <AlertDialog {...dialogProps}>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. The poll cannot be reopened afterwards
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+
+                    <AlertDialogFooter>
+                        <AlertDialogCancel
+                            disabled={isFieldDisabled}
+                            className={cn(buttonVariants({variant: "ghost"}))}
+                        >
+                            Cancel
+                        </AlertDialogCancel>
+
+                        <AlertDialogAction
+                            disabled={isFieldDisabled}
+                            onClick={handleClose}
+                            className={cn(buttonVariants({variant: "destructive"}))}
+                        >
+                            {isFieldDisabled ? "Closing..." : "Close"}
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        );
     };
-
-    const isFieldDisabled = false;
-
-    return (
-        <AlertDialog {...dialogProps}>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        This action cannot be undone. The poll cannot be reopened afterwards
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-
-                <AlertDialogFooter>
-                    <AlertDialogCancel
-                        disabled={isFieldDisabled}
-                        className={cn(buttonVariants({variant: "ghost"}))}
-                    >
-                        Cancel
-                    </AlertDialogCancel>
-
-                    <AlertDialogAction
-                        disabled={isFieldDisabled}
-                        onClick={handleClose}
-                        className={cn(buttonVariants({variant: "destructive"}))}
-                    >
-                        {isFieldDisabled ? "Closing..." : "Close"}
-                    </AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    );
-};
